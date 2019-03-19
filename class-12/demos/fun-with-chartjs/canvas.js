8a014b96544f448990743043a6881b217eeee2a8
Nicholas Carignan <nick.carignan@sbcglobal.net>
2019-03-19 12:12:05 -0700

    demos day 12

---
+++ class-12/demos/fun-with-chartjs/canvas.js
@@ -0,0 +1,49 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 50, 200);
+ctx.fillRect(100, 10, 50, 200);
+ctx.fillRect(190, 10, 50, 150);
+ctx.fillRect(280, 10, 50, 300);
+var myChart = new Chart(ctx, {
+  type: 'radar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    animation: {
+      duration: 10000,
+      easing: 'easeInQuad'
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
